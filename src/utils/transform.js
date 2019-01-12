9f8b6e2fcf425f185db66a8025e308ad7d880677
whynotcrybot <alexanderkatsero@gmail.com>
2019-01-12 19:46:19 +0200

    return array of arrays of numbers

--- src/utils/transform.js
+++ src/utils/transform.js
@@ -1,7 +1,7 @@
 const { intToBin } = require('./convert');
 
 function transformToArrayOfArrays(picture) {
-  return picture.map(row => intToBin(row).split(''));
+  return picture.map(row => intToBin(row).split('').map(x => parseInt(x)));
 }
 
 function shift(picture, step) {
