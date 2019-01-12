528e44db822bc7484161221371e343fdb71ae6c4
whynotcrybot <alexanderkatsero@gmail.com>
2019-01-12 19:12:59 +0200

    improve terminal and matrix

--- src/lib/Matrix.js
+++ src/lib/Matrix.js
@@ -1,25 +1,53 @@
+const { MATRIX_CHARS } = require('./chars');
+
 const IS_ARM_ARCH = process.arch === 'arm';
+const DEFAULT_PICTURE = [
+  [0, 0, 0, 0, 0, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 1, 1, 0, 1, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1, 1],
+  [0, 0, 1, 0, 0, 0, 0, 0],
+  [1, 0, 1, 1, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0],
+];
 
 const invert = bits => bits.map(bit => bit === 0 ? 1 : 0);
 
-module.exports = class Matrix {
+function toMatrixFormat (row, cols) {
+  if (cols.every(col => col === 0)) {
+    return [];
+  }
+
+  const row_swap_rule = [9 , 14, 8 , 12, 1 , 7 , 2 , 5];
+  const col_swap_rule = [13, 3 , 4 , 10, 6 , 11, 15, 16];
+
+  const col_swap = invert(cols);
+
+  const result = new Array(16).fill(0);
+
+  result[row_swap_rule[row] - 1] = 1;
+  col_swap.forEach((value, i) => {
+    result[col_swap_rule[i] - 1] = value;
+  })
+
+  const result_p1 = result.splice(0, 8).reverse();
+  const result_p2 = result.splice(-8, 8);
+
+  return [
+    parseInt( result_p2.join('') , 2),
+    parseInt( result_p1.join('') , 2),
+  ];
+}
+
+class Matrix {
   constructor (events, register) {
     this.register = register;
     this.events = events;
 
-    this.picture = [
-      [0, 0, 0, 0, 0, 1, 1, 1],
-      [0, 0, 0, 0, 0, 0, 0, 1],
-      [0, 0, 0, 0, 0, 0, 1, 0],
-      [0, 0, 1, 1, 0, 1, 0, 0],
-      [0, 0, 0, 1, 0, 1, 1, 1],
-      [0, 0, 1, 0, 0, 0, 0, 0],
-      [1, 0, 1, 1, 0, 0, 0, 0],
-      [1, 0, 0, 0, 0, 0, 0, 0],
-    ];
 
     this.subscribe();
-    this.display();
+    this.setPicture(DEFAULT_PICTURE);
   }
 
   display () {
@@ -40,33 +68,10 @@ module.exports = class Matrix {
     this.display();
   }
 
-  toMatrixFormat (row, cols) {
-    if (cols.every(col => col === 0)) {
-      return [];
-    }
-
-    const row_swap_rule = [9 , 14, 8 , 12, 1 , 7 , 2 , 5];
-    const col_swap_rule = [13, 3 , 4 , 10, 6 , 11, 15, 16];
-
-    const col_swap = invert(cols);
-
-    const result = new Array(16).fill(0);
-
-    result[row_swap_rule[row] - 1] = 1;
-    col_swap.forEach((value, i) => {
-      result[col_swap_rule[i] - 1] = value;
-    })
-
-    const result_p1 = result.splice(0, 8).reverse();
-    const result_p2 = result.splice(-8, 8);
-
-    return [
-      parseInt( result_p2.join('') , 2),
-      parseInt( result_p1.join('') , 2),
-    ];
-  }
 
   subscribe () {
-    this.events.on('display', (picture) => this.setPicture(picture));
+    this.events.on('draw', (picture) => this.setPicture(picture));
   }
 }
+
+module.exports = Matrix;
--- src/lib/Terminal.js
+++ src/lib/Terminal.js
@@ -4,12 +4,16 @@ const SPACE = ' ';
 const BLACK_CELL = colors.bgBlack(SPACE);
 const RED_CELL = colors.bgRed(SPACE);
 
-const isOne = (x) => x === 1;
- 
+const isOne = (x) => parseInt(x) === 1;
+
 class Terminal {
   send(picture) {
     for(const row of picture) {
-      console.log(row.map(cell => isOne(cell) ? RED_CELL : BLACK_CELL).join(SPACE))
+      const formattedString = row
+        .map(cell => isOne(cell) ? RED_CELL : BLACK_CELL)
+        .join(SPACE);
+
+      console.log(formattedString);
     }
   }
 }
